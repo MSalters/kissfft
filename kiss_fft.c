c37d83bb59537c4eb39d91c39183ec1cc853e699
Michiel Salters <Michiel.Salters@SoundIntel.com>
2021-12-23 16:25:44 +0100

    Hoist st_inverse check from kf_bfly4

--- kiss_fft.c
+++ kiss_fft.c
@@ -6,7 +6,7 @@
  *  See COPYING file for more information.
  */
 
-
+#include <stdio.h>
 #include "_kiss_fft_guts.h"
 /* The guts header contains all the multiplication and addition macros that are defined for
  fixed or floating point complex numbers.  It also delares the kf_ internal functions.
@@ -38,40 +38,35 @@ static void kf_bfly4(
     kiss_fft_cpx *tw1,*tw2,*tw3;
     kiss_fft_cpx scratch[6];
     size_t k=m;
-    const size_t m2=2*m;
-    const size_t m3=3*m;
 
+    tw3 = tw2 = tw1 = st->twiddles; // tw2 is just tw1^2, tw3=tw1^3=tw1*tw2
 
-    tw3 = tw2 = tw1 = st->twiddles;
+    // Hoist st->inverse check
+    const size_t m_inv = st->inverse ? 3*m : m;
+    const size_t m3_inv = st->inverse ? m : 3*m;
 
     do {
         C_FIXDIV(*Fout,4); C_FIXDIV(Fout[m],4); C_FIXDIV(Fout[m2],4); C_FIXDIV(Fout[m3],4);
 
         C_MUL(scratch[0],Fout[m] , *tw1 );
-        C_MUL(scratch[1],Fout[m2] , *tw2 );
-        C_MUL(scratch[2],Fout[m3] , *tw3 );
+        C_MUL(scratch[1],Fout[2*m] , *tw2 );
+        C_MUL(scratch[2],Fout[3*m] , *tw3 );
 
         C_SUB( scratch[5] , *Fout, scratch[1] );
         C_ADDTO(*Fout, scratch[1]);
         C_ADD( scratch[3] , scratch[0] , scratch[2] );
         C_SUB( scratch[4] , scratch[0] , scratch[2] );
-        C_SUB( Fout[m2], *Fout, scratch[3] );
+        C_SUB( Fout[2*m], *Fout, scratch[3] );
         tw1 += fstride;
         tw2 += fstride*2;
         tw3 += fstride*3;
         C_ADDTO( *Fout , scratch[3] );
 
-        if(st->inverse) {
-            Fout[m].r = scratch[5].r - scratch[4].i;
-            Fout[m].i = scratch[5].i + scratch[4].r;
-            Fout[m3].r = scratch[5].r + scratch[4].i;
-            Fout[m3].i = scratch[5].i - scratch[4].r;
-        }else{
-            Fout[m].r = scratch[5].r + scratch[4].i;
-            Fout[m].i = scratch[5].i - scratch[4].r;
-            Fout[m3].r = scratch[5].r - scratch[4].i;
-            Fout[m3].i = scratch[5].i + scratch[4].r;
-        }
+        Fout[m_inv].r = scratch[5].r + scratch[4].i;
+        Fout[m_inv].i = scratch[5].i - scratch[4].r;
+        Fout[m3_inv].r = scratch[5].r - scratch[4].i;
+        Fout[m3_inv].i = scratch[5].i + scratch[4].r;
+
         ++Fout;
     }while(--k);
 }
